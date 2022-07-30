48a53247f2b3ef0895c3f26476ad269e8ca8661d
GHunor <45698982+GHunor@users.noreply.github.com>
2022-07-30 20:30:46 +0200

    Two functions added

--- password_manager.py
+++ password_manager.py
@@ -1,12 +1,18 @@
+def view():
+    pass
+def add():
+    addrs = input("Address: ")
+    pwrd = input("Password: ")
+
 mpwrd = input("Please give me the master password")
-print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n"
+print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n")
 
 mode = input(">: ").lower()
-if mode = "quit":
-    quit
+if mode == "quit":
+    quit()
 if mode == "view":
     pass
 elif mode == "add":
     pass
 else:
-    print("Invalid mode"
+    print("Invalid mode")
