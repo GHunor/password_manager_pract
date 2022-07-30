ff85717b0f5ac8d8acb3080a2602c89037c78fb4
GHunor <45698982+GHunor@users.noreply.github.com>
2022-07-30 23:27:22 +0200

    Not tested

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,35 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+def load_key():
+    key_file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            uname,pwrd = data.split("|")
+            print("User: " + uname + " | Password: " + fer.decrypt(pwrd.encode()).decode())
 def add():
-    addrs = input("Address: ")
+    name = input("Address: ")
     pwrd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(addrs + "|" + pwrd + "\n")
+        f.write(name + "|" + fer.encrypt(pwrd.encode()).decode() + "\n")
 
-mpwrd = input("Please give me the master password")
+
+mpwrd = input("Please give me the master password ")
+write_key()
+key = load_key() + mpwrd.encode()
+fer = fernet(key)
+
 print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n")
 
 mode = input(">: ").lower()
