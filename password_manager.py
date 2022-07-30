208a89d2a59123cfadd1d25e0a3e04d9d7e7d30b
GHunor <45698982+GHunor@users.noreply.github.com>
2022-07-30 20:13:12 +0200

    First instance

---
+++ password_manager.py
@@ -0,0 +1,12 @@
+mpwrd = input("Please give me the master password")
+print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n"
+
+mode = input(">: ").lower()
+if mode = "quit":
+    quit
+if mode == "view":
+    pass
+elif mode == "add":
+    pass
+else:
+    print("Invalid mode"
