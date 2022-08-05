d01c8be35143639340bf02e95ad1389b56dbbe4a
GHunor <45698982+GHunor@users.noreply.github.com>
2022-08-05 10:53:52 +0200

    Main function Structured

--- password_manager.py
+++ password_manager.py
@@ -23,21 +23,27 @@ def add():
 
     with open('passwords.txt', 'a') as f:
         f.write(name + "|" + fer.encrypt(pwrd.encode()).decode() + "\n")
+def promt():
+    print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n")
 
-
-mpwrd = input("Please give me the master password ")
-write_key()
-key = load_key() + mpwrd.encode()
-fer = fernet(key)
-
-print("What would you like to do?\nView already saved passwords? (view)\nAdd new password? (add)\nQuit? (quit)\n")
-
-mode = input(">: ").lower()
-if mode == "quit":
-    quit()
-if mode == "view":
-    view()
-elif mode == "add":
-    add()
-else:
-    print("Invalid mode")
+    mode = input(">: ").lower()
+    if mode == "quit":
+        quit()
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        
+def main():
+    mpwrd = input("Please give me the master password ")
+    write_key()
+    key = load_key() + mpwrd.encode()
+    fer = fernet(key)
+    promt()
+    
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
